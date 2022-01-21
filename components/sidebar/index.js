e0591f382cc150a4c22455864524d261d55d3eb9
rock-zion <rock_zion@outlook.com>
2022-01-21 14:28:31 +0100

    improve readability with LinkItem component

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,3 +1,34 @@
+const LinkItem = React.forwardRef(
+  ({ getActiveColor, linkIsActive, text, onClick, IMG, href }, ref) => {
+    //
+    const link_wrapper = classNames('d-flex', 'px-3', style.link_wrapper, {
+      'bg-primary_light text-dark': linkIsActive(href),
+    });
+
+    const link = classNames(
+      style.link,
+      'text-secondary',
+      'd-flex',
+      'align-items-center',
+      {
+        'text-dark': linkIsActive(href),
+      }
+    );
+
+    return (
+      <div className={link_wrapper} ref={ref}>
+        <div className='d-flex'>
+          <div className='mr-3 align-items-center'>
+            <IMG fill={getActiveColor(href)} />
+          </div>
+          <a className={link} onClick={onClick} href={href}>
+            {text}
+          </a>
+        </div>
+      </div>
+    );
+  }
+);
 Sidebar.defaultProps = {
   sidebar_links: [
     { title: 'Overview', url: '/overview', IMG: Overview },
