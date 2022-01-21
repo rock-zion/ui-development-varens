4724a6a77bc993bffb05fc9032e6d2daa0349725
rock-zion <rock_zion@outlook.com>
2022-01-21 14:31:44 +0100

    set up _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'styles/globals.scss';
+import 'styles/tabs.scss';
+import { useEffect } from 'react';
+import Layout from 'components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
