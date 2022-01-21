7b6af2216aa0177c5b6de40477bac193feb39cec
rock-zion <rock_zion@outlook.com>
2022-01-21 14:45:41 +0100

    created DoubleRowDetail component

--- components/profiles/profileimages.js
+++ components/profiles/profileimages.js
@@ -3,3 +3,11 @@
     { [`${style.sm_img_wrapper}`]: size === 'sm' },
     { [`${style.lg_img_wrapper}`]: size === 'lg' }
   );
+export const DoubleRowDetail = ({ detail1, detail2 }) => {
+  return (
+    <div className={classNames('d-flex', 'ml-2', ' flex-column')}>
+      <span className={style.detail1}>{detail1}</span>
+      <span className={style.detail2}>{detail2}</span>
+    </div>
+  );
+};
