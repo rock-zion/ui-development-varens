63e22fedcd9e9372edeb96b35527fea1bb12b77d
rock-zion <rock_zion@outlook.com>
2022-01-21 14:51:28 +0100

    create dummy data for overview

---
+++ components/overview/index.js
@@ -0,0 +1,36 @@
+Overview.defaultProps = {
+  overview_data: [
+    {
+      id: '1',
+      Img: Sales,
+      title: 'Total sales',
+      amount: '$391,820.75',
+      profit: { status: true, rate: 21.9 },
+      daily_amount: '$67k',
+    },
+    {
+      id: '2',
+      Img: Visitor,
+      title: 'Visitor',
+      amount: '831,071',
+      profit: { status: true, rate: 13 },
+      daily_amount: '7k',
+    },
+    {
+      id: '3',
+      Img: Orders,
+      title: 'Total orders',
+      amount: '1,042,665',
+      profit: { status: true, rate: 5.7 },
+      daily_amount: '5k',
+    },
+    {
+      id: '4',
+      Img: Refunded,
+      title: 'Refunded',
+      amount: '50,441',
+      profit: { status: false, rate: 5.7 },
+      daily_amount: '5k',
+    },
+  ],
+};
