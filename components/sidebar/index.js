7d6dc54a4b6ee6ace812c20bc17e7378535f9ece
rock-zion <rock_zion@outlook.com>
2022-01-21 14:27:37 +0100

    create default date for sidebar

---
+++ components/sidebar/index.js
@@ -0,0 +1,13 @@
+Sidebar.defaultProps = {
+  sidebar_links: [
+    { title: 'Overview', url: '/overview', IMG: Overview },
+    { title: 'Product', url: '/products', IMG: Products },
+    { title: 'Orders', url: '/orders', IMG: Orders },
+    { title: 'Customers', url: '/customers', IMG: Customers },
+    { title: 'Manage Reviews', url: '/managereviews', IMG: ManageReviews },
+    { title: 'Checkout', url: '/checkout', IMG: Checkout },
+    { title: 'Settings', url: '/settings', IMG: Setting },
+  ],
+};
+
+// https://stackoverflow.com/questions/54519654/how-do-i-add-color-to-my-svg-image-in-react
