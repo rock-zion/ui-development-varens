8ee98ace47e5401ae6faa78aa417ad2c93464e82
rock-zion <rock_zion@outlook.com>
2022-01-21 14:46:04 +0100

    when no image exists display initials

--- components/profiles/profileimages.js
+++ components/profiles/profileimages.js
@@ -3,6 +3,14 @@
     { [`${style.sm_img_wrapper}`]: size === 'sm' },
     { [`${style.lg_img_wrapper}`]: size === 'lg' }
   );
+      {!src && (
+        <div className={classNames(sizes)}>
+          <span className='display-1 fw-bold'>
+            {first_name[0]}
+            {last_name[0]}
+          </span>
+        </div>
+      )}
 export const DoubleRowDetail = ({ detail1, detail2 }) => {
   return (
     <div className={classNames('d-flex', 'ml-2', ' flex-column')}>
