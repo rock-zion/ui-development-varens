51fdd125a1cfd7c8f867a1e91140242fdb9c67f9
rock-zion <rock_zion@outlook.com>
2022-01-21 15:01:32 +0100

    create dummy data

---
+++ components/trafficchannels/country_traffic_channels.js
@@ -0,0 +1,30 @@
+CountryTrafficChannels.defaultProps = {
+  table_header: ['STATES', 'ORDERS', 'SALES'],
+  table_content: [
+    {
+      country: 'California',
+      orders: 12202,
+      sales: 150200,
+    },
+    {
+      country: 'Texas',
+      orders: 11950,
+      sales: 147715,
+    },
+    {
+      country: 'New York',
+      orders: 11198,
+      sales: 120322,
+    },
+    {
+      country: 'Washington',
+      orders: 9885,
+      sales: 119008,
+    },
+    {
+      country: 'Kansas',
+      orders: 8560,
+      sales: 102803,
+    },
+  ],
+};
