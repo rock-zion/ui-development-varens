929f8b1de2235966bd778c6b4d0f1a004f2f3d05
rock-zion <rock_zion@outlook.com>
2022-01-21 15:04:26 +0100

    Update overview.js

--- pages/overview.js
+++ pages/overview.js
@@ -1,5 +1,129 @@
+import { Header } from 'components/header';
+import { Overview } from 'components/overview';
+import style from 'styles/overview.module.scss';
+import classNames from 'classnames';
+import { RevenueVsOrders } from 'components/revenue-vs-orders';
+import { TrafficChannels } from 'components/trafficchannels';
+import { Transaction } from 'components/cards/transaction';
+import { NewCustomer } from 'components/profiles/new_customer';
+import { TransactionHistory } from 'components/profiles/transaction_history';
+import { BarChart } from 'components/charts/barchart';
+import { default as SmGain } from 'assets/SmGain.svg';
+import { default as SmLoss } from 'assets/SmLoss.svg';
+import { CountryTrafficChannels } from 'components/trafficchannels/country_traffic_channels';
+import { TopProducts } from 'components/trafficchannels/topproducts';
+import { Searchbar } from 'components/header/searchbar';
+
+export default function Home({
+  new_customers_data: nc_data,
+  transaction_history_data: th_data,
+}) {
+  const revenue_row_wrapper = classNames(
+    style.revenue_row_wrapper,
+    'mt-3',
+    'mb-2'
+  );
+
+  const customers_row_wrapper = classNames(style.customers_row_wrapper, 'mt-3');
+  return (
+    <div>
       <Searchbar />
       <Header />
+      <Overview />
+      <div className={revenue_row_wrapper}>
+        <RevenueVsOrders />
+        <TrafficChannels />
+      </div>
+      <div className={style.traffic_channels_wrapper}>
+        <CountryTrafficChannels />
+        <TopProducts />
+      </div>
+      <div className={customers_row_wrapper}>
+        <Transaction
+          link={nc_data.link}
+          hasToolbar
+          hasFooter
+          title='New Customers'>
+          {nc_data.data.map(item => (
+            <NewCustomer
+              key={item.customer_id}
+              profile_image={item?.profile_image}
+              first_name={item?.first_name}
+              last_name={item?.last_name}
+              customer_id={item?.customer_id}
+            />
+          ))}
+        </Transaction>
+
+        <Transaction
+          link={th_data.link}
+          hasToolbar
+          hasFooter
+          title='Transaction Customers'>
+          {th_data.data.map(item => (
+            <TransactionHistory
+              date_time={item?.date_time}
+              key={item?.invoice_no}
+              invoice_no={item?.invoice_no}
+              status={item?.status}
+              remark={item?.remark}
+              amount={item?.amount}
+            />
+          ))}
+        </Transaction>
+        <Transaction title='Real-Time Sale'>
+          <div className='d-flex justify-content-between mb-4'>
+            <div className='d-flex flex-column'>
+              <span className='text-secondary display-2 fw-normal mb-1'>
+                Refunded
+              </span>
+              <div className='d-flex align-items-center'>
+                <span className='fw-bolder display-4 mr-1'> $160,200</span>
+                <SmGain />
+                <span className='display-1 fw-normal ml-1'>0.20%</span>
+              </div>
+            </div>
+            <div className='d-flex flex-column'>
+              <span className='text-secondary display-2 fw-normal mb-1'>
+                Avg. Sales per day
+              </span>
+              <div className='d-flex align-items-center'>
+                <span className='fw-bolder display-4 mr-1'>$22,180</span>
+                <SmLoss />
+                <span className='display-1 fw-normal ml-1'>1.04%</span>
+              </div>
+            </div>
+          </div>
+          <div className={style.barchart_wrapper}>
+            <BarChart />
+          </div>
+          <div className='d-flex align-items-center justify-content-center'>
+            <span
+              className={classNames(
+                style.labels_tday,
+                'd-flex',
+                'align-items-center',
+                'display-2'
+              )}>
+              Today
+            </span>
+            <span
+              className={classNames(
+                style.labels_yday,
+                'd-flex',
+                'align-items-center',
+                'display-2',
+                'ml-3'
+              )}>
+              Yesterday
+            </span>
+          </div>
+        </Transaction>
+      </div>
+    </div>
+  );
+}
+
 Home.defaultProps = {
   new_customers_data: {
     link: {
