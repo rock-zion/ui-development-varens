e046d316b4d228f52b3fca68fa92822603359b4d
rock-zion <rock_zion@outlook.com>
2022-01-21 14:46:12 +0100

    Update profileimages.js

--- components/profiles/profileimages.js
+++ components/profiles/profileimages.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import classNames from 'classnames';
+import style from './profile.module.scss';
+
+export const ProfileImage = ({ size, src, first_name, last_name }) => {
   const sizes = classNames(
     'bg-light_blue text-white',
     { [`${style.sm_img_wrapper}`]: size === 'sm' },
     { [`${style.lg_img_wrapper}`]: size === 'lg' }
   );
+  return (
+    <>
+      {src && (
+        <div className={classNames(sizes)}>
+          {/* eslint-disable-next-line */}
+          <img alt="" src={src} />
+        </div>
+      )}
       {!src && (
         <div className={classNames(sizes)}>
           <span className='display-1 fw-bold'>
@@ -11,6 +24,10 @@
           </span>
         </div>
       )}
+    </>
+  );
+};
+
 export const DoubleRowDetail = ({ detail1, detail2 }) => {
   return (
     <div className={classNames('d-flex', 'ml-2', ' flex-column')}>
