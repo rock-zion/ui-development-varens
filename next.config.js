95220a7bdca8cfd06f25217d6454b9bb8e2929dc
rock-zion <rock_zion@outlook.com>
2022-01-21 14:31:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-  reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [
+      'node_modules/breakpoint-sass/stylesheets',
+      path.join(__dirname, 'styles'),
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __DEV__: process.env.NODE_ENV === 'development',
+      })
+    );
+
+    return config;
+  },
+};
