0bbf82dd6cdec54448a683e674c71ceb2c9c7ab0
rock-zion <rock_zion@outlook.com>
2022-01-21 14:28:56 +0100

    import assets

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,3 +1,11 @@
+import { default as Overview } from 'assets/sidebar/Overview.svg';
+import { default as Products } from 'assets/sidebar/Products.svg';
+import { default as Orders } from 'assets/sidebar/Orders.svg';
+import { default as Customers } from 'assets/sidebar/Customers.svg';
+import { default as ManageReviews } from 'assets/sidebar/ManageReviews.svg';
+import { default as Checkout } from 'assets/sidebar/Checkout.svg';
+import { default as Setting } from 'assets/sidebar/Settings.svg';
+import { default as Brand } from 'assets/sidebar/Logo.svg';
 const LinkItem = React.forwardRef(
   ({ getActiveColor, linkIsActive, text, onClick, IMG, href }, ref) => {
     //
